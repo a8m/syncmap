47611cd49b3847cfed2456fecccaceeab8401b46
Owen <owensmith0031@gmail.com>
2019-10-10 21:49:52 +0100

    Enable the generation of a map[T](chan T) (#3)
    * Added tests for chan types
    
    * go fmt
    
    * Added types to allow for map[T](chan T2)
    
    * Removed line after switch case

--- testdata/gen_test.go
+++ testdata/gen_test.go
@@ -80,3 +80,79 @@ func TestRequests(t *testing.T) {
 		return true
 	})
 }
+
+func TestStringByteChan(t *testing.T) {
+	var m StringByteChan
+	m.Store("r", make(chan []byte))
+	_, ok := m.Load("r")
+	if !ok {
+		t.Fatal("value should be existed")
+	}
+	m.Delete("r")
+	_, ok = m.Load("r")
+	if ok {
+		t.Fatal("value should not be existed")
+	}
+	r, loaded := m.LoadOrStore("r", make(chan []byte))
+	if loaded {
+		t.Fatal("value should not be loaded")
+	}
+	lr, loaded := m.LoadOrStore("r", r)
+	if !loaded {
+		t.Fatal("value should not be loaded")
+	}
+	if lr != r {
+		t.Fatal("loaded value should be the same")
+	}
+	s, _ := m.LoadOrStore("s", make(chan []byte))
+	kv := map[string](chan []byte){"r": r, "s": s}
+	m.Range(func(key string, value chan []byte) bool {
+		v, ok := kv[key]
+		if !ok {
+			t.Fatal("keys do not match")
+		}
+		if value != v {
+			t.Fatal("values do not match")
+		}
+		delete(kv, key)
+		return true
+	})
+}
+
+func TestStringIntChan(t *testing.T) {
+	var m StringIntChan
+	m.Store("r", make(chan int))
+	_, ok := m.Load("r")
+	if !ok {
+		t.Fatal("value should be existed")
+	}
+	m.Delete("r")
+	_, ok = m.Load("r")
+	if ok {
+		t.Fatal("value should not be existed")
+	}
+	r, loaded := m.LoadOrStore("r", make(chan int))
+	if loaded {
+		t.Fatal("value should not be loaded")
+	}
+	lr, loaded := m.LoadOrStore("r", r)
+	if !loaded {
+		t.Fatal("value should not be loaded")
+	}
+	if lr != r {
+		t.Fatal("loaded value should be the same")
+	}
+	s, _ := m.LoadOrStore("s", make(chan int))
+	kv := map[string](chan int){"r": r, "s": s}
+	m.Range(func(key string, value chan int) bool {
+		v, ok := kv[key]
+		if !ok {
+			t.Fatal("keys do not match")
+		}
+		if value != v {
+			t.Fatal("values do not match")
+		}
+		delete(kv, key)
+		return true
+	})
+}
